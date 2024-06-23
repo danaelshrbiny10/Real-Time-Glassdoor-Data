1e988137a2dd1c1360c9f1bb4de3ace9bb957db6
Dana <danaelshrbiny55@gmail.com>
2024-06-24 01:42:01 +0300

    remove static and templates

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('.read-more').forEach(item => {
-        item.addEventListener('click', event => {
-            event.preventDefault();
-            const fullText = item.getAttribute('data-full-text');
-            item.parentElement.innerHTML = fullText;
-        });
-    });
-});
